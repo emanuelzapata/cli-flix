bb345a6fd09fffc48276e1d518aa312a23a63b74
Emanuel Zapata <emanuelzapata@MacBook-Pro.local>
2025-11-05 19:36:58 -0600

    fix: initial commit of working input flags and basic project configuration

---
+++ cmd/root.go
@@ -0,0 +1,85 @@
+/*
+Copyright © 2025 Emanuel Zapata
+*/
+package cmd
+
+import (
+	"os"
+
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var title string
+var year int
+var apikey string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "cli-flix",
+	Short: "🎬  Your personal IMDB in the terminal!",
+	Long: `
+───────────────────────────────────────────────────
+🍿   CLI-Flix — Movie & TV Info Without Leaving Code
+───────────────────────────────────────────────────
+
+Ever been deep in a coding flow and thought:
+“Man, I could use a movie break...”
+
+CLI-Flix has you covered.
+
+✨  Search movies and shows by name
+🎥  Instantly fetch details, ratings, and release info
+🧠  Decide if it's worth your precious downtime
+
+Examples:
+  $ cli-flix -t "Star Wars: The Phantom Menace"
+  $ cli-flix --title "The Matrix"
+
+───────────────────────────────────────────────────
+Because even developers deserve a good movie break.
+🍿
+`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) {
+		if apikey != "" {
+			fmt.Println(apikey)
+		} else if title != "" && year != 0 {
+			fmt.Println("Movie was given with a year")
+			fmt.Println(title)
+			fmt.Println(year)
+		} else if title != "" {
+			fmt.Println("Movie title was given only")
+		} else if title == "" && year != 0 {
+			fmt.Println("Only year was given, please give us a title")
+		}
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cli-flix.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+
+	rootCmd.Flags().StringVarP(&title, "title", "t", "", "Movie or TV Show Title")
+	rootCmd.Flags().IntVarP(&year, "year", "y", 0, "Year of the TV Show or Movie")
+	rootCmd.Flags().StringVarP(&apikey, "login", "", "", "Login Using Users API Key")
+
+	rootCmd.Flags().BoolP("help", "h", false, "Get help")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2025 Emanuel Zapata
+
+*/
+package main
+
+import "cli-flix/cmd"
+
+func main() {
+	cmd.Execute()
+}
