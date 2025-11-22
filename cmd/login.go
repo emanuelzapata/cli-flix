f74a0872f4d582de22ff3d5a8909b42ae99213b2
Emanuel Zapata <emanuelzapata@MacBook-Pro.local>
2025-11-22 11:41:34 -0600

    fix: added in new comments

--- cmd/login.go
+++ cmd/login.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+Copyright © 2025 Emanuel Zapata
 */
 package cmd
 
@@ -15,13 +15,9 @@ var apiKey string
 // loginCmd represents the login command
 var loginCmd = &cobra.Command{
 	Use:   "login",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Login command to set your API key",
+	Long: `Use this login command to set your local API key for OMDB which can be gotten from https://www.omdbapi.com/ 
+	// Currently this version of cli-flix only supports OMDB but other services will be implemented soon!`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("login called")
 		fmt.Println(apiKey)
