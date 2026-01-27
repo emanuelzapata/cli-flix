0e1397011c1ce84b2ca5b5547900d2ccebca4cbe
Emanuel Zapata <emanuelzapata@MacBook-Pro.local>
2026-01-26 19:52:50 -0600

    fix: added in readin config for env file

---
+++ cmd/recommend.go
@@ -0,0 +1,46 @@
+/*
+Copyright © 2026 Emanuel Zapata
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var random bool
+
+// recommendCmd represents the recommend command
+var recommendCmd = &cobra.Command{
+	Use:   "recommend",
+	Short: "This command will recommend movies",
+	Long:  `Use this command to get recommendations on what to watch!`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		viper.ReadInConfig()
+		fmt.Println("recommend called")
+		if random {
+			fmt.Println("passed in value")
+			apiKey := viper.GetString("THE_MOVIE_DB_API_KEY")
+			if apiKey == "" {
+				fmt.Println("Please set an API key")
+			} else {
+				fmt.Println("Making request")
+				getRandomRecommendation(apiKey)
+			}
+		}
+	},
+}
+
+func getRandomRecommendation(apiKey string) string {
+	fmt.Println(apiKey)
+	return "Random"
+}
+
+func init() {
+	rootCmd.AddCommand(recommendCmd)
+
+	recommendCmd.Flags().BoolVarP(&random, "random", "r", false, "Get a random recommendation")
+}
